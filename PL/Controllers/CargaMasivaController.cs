88562e9c99231950601fe84fddeffbb548d5fd44
iespinoza96 <iespinoza@digis01.com>
2022-11-17 12:25:48 -0600

    CargaMAsiva TXT

---
+++ PL/Controllers/CargaMasivaController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace PL.Controllers
+{
+    public class CargaMasivaController : Controller
+    {
+        [HttpGet]
+        public ActionResult CargaMasiva()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CargaTXT()
+        {
+            IFormFile fileTXT = Request.Form.Files["archivoTXT"];
+
+            
+            if (fileTXT != null)
+            {
+
+                StreamReader Textfile = new StreamReader(fileTXT.OpenReadStream());
+
+                string line;
+                line = Textfile.ReadLine();
+                while ((line = Textfile.ReadLine()) != null)
+                {
+                    string[] lines = line.Split('|');
+
+                    ML.Alumno alumno = new ML.Alumno();
+
+                    alumno.Nombre = lines[0];
+                    alumno.ApellidoPaterno = lines[1];
+                    alumno.ApellidoMaterno = lines[2];
+                    alumno.FechaNacimiento = lines[3];
+                    alumno.Sexo = lines[4];
+
+                    alumno.Semestre = new ML.Semestre();
+                    alumno.Semestre.IdSemestre = byte.Parse(lines[5]);
+                    alumno.Imagen = null;
+
+
+
+                    ML.Result result = BL.Alumno.Add(alumno);
+
+
+                    if (!result.Correct)
+                    {
+                        string fileError = @"C:\Users\digis\Documents\Isaac Jair Espinoza Ocampo\BlocdeNotas\ErroresLayout.txt";
+                        //result.ErrorMessage;
+                        StreamWriter errorFile = new StreamWriter(fileError);
+                        //CREAR UN TXT DE ERRORES
+                    }
+                    else
+                    {
+                      
+
+                    }
+
+                }
+            }
+            return PartialView("Modal");
+        }
+    }
+}
---
+++ PL_C/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+ReadFile();
+Console.ReadKey();
+
+
+static void ReadFile()
+{
+    string file = @"C:\Users\digis\Documents\Isaac Jair Espinoza Ocampo\BlocdeNotas\LayoutAlumnos.txt";
+
+    if (File.Exists(file))
+    {
+        
+        StreamReader Textfile = new StreamReader(file);
+        
+        string line;
+        line = Textfile.ReadLine();
+        while ((line = Textfile.ReadLine()) != null)
+        {
+            string[] lines = line.Split('|');
+
+            ML.Alumno alumno = new ML.Alumno();
+
+            alumno.Nombre = lines[0];
+            alumno.ApellidoPaterno = lines[1];
+            alumno.ApellidoMaterno = lines[2];
+            alumno.FechaNacimiento = lines[3];
+            alumno.Sexo = lines[4];
+
+            alumno.Semestre = new ML.Semestre();
+            alumno.Semestre.IdSemestre = byte.Parse(lines[5]);
+            alumno.Imagen = null;
+
+
+
+            ML.Result result = BL.Alumno.Add(alumno);
+
+
+            if (result.Correct)
+            {
+                Console.WriteLine("Correcto");
+                Console.ReadKey();
+            }
+            else
+            {
+                string fileError = @"C:\Users\digis\Documents\Isaac Jair Espinoza Ocampo\BlocdeNotas\ErroresLayout.txt";
+                //result.ErrorMessage;
+                StreamWriter errorFile = new StreamWriter(fileError);
+                //CREAR UN TXT DE ERRORES
+
+            }
+
+        }
+    }
+}
